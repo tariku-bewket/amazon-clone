786d8c00ff40939bc8d73464ba9faa653588f8d1
Tariku Bewket <tarikubew@gmail.com>
2024-04-11 14:26:22 +0100

    connect backend server to frontend

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:5001/clone-53304/us-central1/api',
+  // Local instance of firebase functions
+  // baseURL: 'http://127.0.0.1:5001/clone-53304/us-central1/api',
+
+  // deployed version of amazon server on render.com
+  baseURL: 'https://amazon-api-deploy-d2p7.onrender.com/',
 });
 
 export { axiosInstance };
